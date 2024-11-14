1c8ffc37de7a602448ca4f968687eb92690fc291
YU-CHE LIN <2400647@uad.ac.uk>
2024-11-14 11:09:50 +0000

    new cursor

--- DES510/Assets/Scripts/Others/CustomCursor.cs
+++ DES510/Assets/Scripts/Others/CustomCursor.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class CustomCursor : MonoBehaviour
 {
-    [SerializeField] private Sprite defaultCursor;
+    [SerializeField] private Texture2D defaultCursor;
 
     private void Awake()
     {
-        Cursor.SetCursor(defaultCursor.texture,Vector2.zero,CursorMode.Auto);
+        Cursor.SetCursor(defaultCursor,Vector2.zero,CursorMode.Auto);
     }
 
 }
